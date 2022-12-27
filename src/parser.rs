fc62b421a7a7b4926b42625e7ee86a1bb649be1a
Balázs Dukai <balazs.dukai@gmail.com>
2022-12-27 19:26:43 +0100

    Compute compressed centroids

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,6 @@
 use serde::Deserialize;
 use zerovec::{VarZeroVec, ZeroSlice};
+use std::path::PathBuf;
 
 /// A partial [CityJSON object](https://www.cityjson.org/specs/1.1.3/#cityjson-object).
 /// It is partial, because we only store the metadata that is necessary for parsing the
@@ -19,7 +20,7 @@ struct Transform {
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 struct Metadata {
-    reference_system: String
+    reference_system: String,
 }
 
 /// Container for storing the CityJSONFeature vertices.
@@ -29,14 +30,56 @@ struct Metadata {
 #[derive(Deserialize, Debug)]
 struct CityJSONFeatureVertices<'a> {
     #[serde(borrow)]
-    vertices: VarZeroVec<'a, ZeroSlice<i64>>
+    vertices: VarZeroVec<'a, ZeroSlice<i64>>,
 }
 
+
+impl CityJSONFeatureVertices<'_> {
+    fn to_feature_tuple(&self) {
+        todo!()
+    }
+
+    /// Return the number of vertices of the feature.
+    /// We assume that the number of vertices in a feature does not exceed 65535 (thus `u16`).
+    fn vertex_count(&self) -> u16 {
+        self.vertices.len() as u16
+    }
+
+    /// Feature centroid (2D) computed as the average coordinate.
+    /// The centroid coordinates are compressed, so they need to be transformed back to real-world
+    /// coordinates.
+    fn centroid(&self) -> (f64, f64) {
+        let mut x_sum: i64 = 0;
+        let mut y_sum: i64 = 0;
+        for v in self.vertices.iter() {
+            x_sum = x_sum + v.get(0).unwrap();
+            y_sum = y_sum + v.get(1).unwrap()
+        }
+        let x = x_sum as f64 / self.vertices.len() as f64;
+        let y = y_sum as f64 / self.vertices.len() as f64;
+        (x, y)
+    }
+}
+
+type FeatureTuple = (u64, u16, PathBuf);
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use serde_json::from_str;
+    use std::fs::read_to_string;
+
+    fn test_data_dir() -> PathBuf {
+        PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+            .join("resources")
+            .join("data")
+    }
+
+    fn test_output_dir() -> PathBuf {
+        PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+            .join("tests")
+            .join("output")
+    }
 
     #[test]
     fn test_cityjsonmetadata() -> serde_json::Result<()> {
@@ -72,6 +115,25 @@ mod tests {
         for v in cf.vertices.iter() {
             println!("{:#?}", v.get(0));
         }
+        Ok(())
+    }
+
+    #[test]
+    fn test_centroid() -> serde_json::Result<()> {
+        let pb: PathBuf = test_data_dir().join("3dbag_feature_x71.city.jsonl");
+        let cf_str = read_to_string(&pb).unwrap();
+        let cf: CityJSONFeatureVertices = from_str(&cf_str)?;
+        let ctr_compressed = cf.centroid();
+        println!("compressed centroid: {:#?}", ctr_compressed);
+
+        let pb: PathBuf = test_data_dir().join("3dbag_x00.city.json");
+        let cm_str = read_to_string(&pb).unwrap();
+        let cm: CityJSONMetadata = from_str(&cm_str)?;
+
+        let ctr_real_world: (f64, f64) = ((ctr_compressed.0 * cm.transform.scale[0]) + cm.transform.translate[0],
+                                          (ctr_compressed.1 * cm.transform.scale[1]) + cm.transform.translate[1]);
+        println!("real-world centroid: {:#?}", ctr_real_world);
+
         Ok(())
     }
 }
