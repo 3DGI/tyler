a0e80bad5b90d70514a224f543e81f58136f82bf
Balázs Dukai <balazs.dukai@gmail.com>
2023-03-16 09:58:45 +0100

    Fix license notices

--- src/proj.rs
+++ src/proj.rs
@@ -3,6 +3,18 @@
 //! coordinates instead of only xy.
 // Copyright 2017 The GeoRust Project Developers https://github.com/georust/proj/blob/main/LICENSE-APACHE
 // Copyright 2023 Balázs Dukai, Ravi Peters
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 use libc::c_int;
 use libc::{c_char, c_double};
--- src/spatial_structs.rs
+++ src/spatial_structs.rs
@@ -1,5 +1,17 @@
 //! Spatial data structures for indexing the features.
 // Copyright 2023 Balázs Dukai, Ravi Peters
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 use crate::parser::FeatureSet;
 use log::{debug, error};
 use std::collections::VecDeque;
